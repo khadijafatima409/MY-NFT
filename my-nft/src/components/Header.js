fb2e51b148194181134ee7e7b07c1cfaf5a63f3a
khadjafatima409 <khadijafatima530@gmail.com>
2023-11-08 13:46:59 -0800

    add intial files

---
+++ my-nft/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="border h-[100vh] bg-[url('')]">
+      <h2>header</h2>
+    </div>
+  );
+};
+// background: url(<path-to-image>), lightgray 0% 0% / 100.84999799728394px 100.84999799728394px repeat;
+//     mix-blend-mode: overlay
+export default Header;
