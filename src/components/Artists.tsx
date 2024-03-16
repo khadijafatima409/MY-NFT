7485740dc9090a73135e4064699a7eb4821d46a3
khadjafatima409 <khadijafatima530@gmail.com>
2024-03-16 21:59:56 +0500

    fix styles

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -6,36 +6,36 @@ const Artists = () => {
   return (
     <div className="w-[80%] mx-auto ">
       <CustomHeader title="Top Artists" className="text-blue" />
-      <div className="max-w-full pt-10 pb-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 border ">
+      <div className="max-w-full pt-10 pb-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 ">
         {Artists_CARD.map((e) => {
           return (
             <>
-              <div className="border flex gap-2">
-                <div className="border  flex ">
-                  <div className="w-10 flex justify-center items-center border">
+              <div className="flex gap-2 ">
+                <div className="flex">
+                  <div className="w-10 flex justify-center items-center ">
                     <p className="text-xl">1</p>
                   </div>
-                  <div className="border relative">
+                  <div className="relative flex justify-center items-center">
                     <Image
                       src="/artist/artist-1.svg"
                       alt=""
                       height={20}
                       width={20}
-                      className="w-14  rounded-full border-4 border-white"
+                      className="w-14 rounded-full border-4 border-white"
                     />
                     <Image
                       src="/artist/Verified.svg"
                       alt=""
                       height={22}
                       width={22}
-                      className="absolute bottom-1 right-0"
+                      className="absolute bottom-4 right-0"
                     />
                   </div>
                 </div>
-                <div className="border flex  ">
-                  <div className="flex flex-col justify-around border h-full font-medium">
+                <div className="w-full flex">
+                  <div className="flex flex-col justify-around h-full w-full font-medium">
                     <h2 className="text-dark-purple">Phuong Pham</h2>
-                    <div className="flex border w-20">
+                    <div className="flex w-20">
                       <Image
                         src="/artist/eth.svg"
                         alt=""
@@ -45,8 +45,8 @@ const Artists = () => {
                       <p className="text-grey-blue">2,798</p>
                     </div>
                   </div>
-                  <div className="border">
-                    <div className="flex justify-end border">
+                  <div className="w-full">
+                    <div className="flex justify-end ">
                       <Image
                         src="/artist/followers.svg"
                         alt=""
@@ -55,8 +55,8 @@ const Artists = () => {
                         className="h-10 w-10"
                       />
                     </div>
-                    <div className="text-purply-blue text-xs ">
-                      <p className="text-right w-20">
+                    <div className="text-purply-blue text-xs w-full flex justify-end">
+                      <p className="text-right  w-24">
                         Follow by mikain and 2,129 users
                       </p>
                     </div>
