be623f2a420cc06ead8ededb520f7a8fcf43e91f
khadjafatima409 <khadijafatima530@gmail.com>
2024-10-10 06:39:43 +0500

    text updated

--- src/components/CustomTabs.tsx
+++ src/components/CustomTabs.tsx
@@ -1,8 +1,5 @@
 "use client";
 import React from "react";
-import Box from "@mui/material/Box";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import CustomButton from "./CustomButton";
 interface CustomTabsProps {
   label1: string;
@@ -10,30 +7,14 @@ interface CustomTabsProps {
   label3: string;
 }
 const CustomTabs = ({ label1, label2, label3 }: CustomTabsProps) => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
   return (
-    // <Box
-    //   sx={{
-    //     height: "50px",
-    //     width: "30%",
-    //     bgcolor: "#CDE2FE",
-    //     border: "1px solid",
-    //   }}
-    // >
-    //   <Tabs value={value} onChange={handleChange} centered>
-    //     <Tab label={label1} />
-    //     <Tab label={label2} />
-    //     <Tab label={label3} />
-    //   </Tabs>
-    // </Box>
-    <div className="bg-aqua flex justify-between items-center h-full w-[300px]">
-      <CustomButton btnText="Tab 1" btnTextClassName=" border" />
-      <CustomButton btnText="Tab 1" />
-      <CustomButton btnText="Tab 1" />
+    <div className="bg-aqua flex justify-between items-center py-2 w-[300px] rounded-lg px-2 text-grey-blue">
+      <CustomButton btnText="Today" btnTextClassName=" " />
+      <CustomButton
+        btnText="This Week"
+        btnTextClassName=" bg-white px-2 py-1 rounded-lg"
+      />
+      <CustomButton btnText="This Month" />
     </div>
   );
 };
